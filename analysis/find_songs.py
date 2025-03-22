8b66897bce06008c53b5f28b4353106f8ecbb9ee
Joe Sheridan <joe@sheridan.lgbt>
2025-03-22 17:19:32 -0400

    Add skeleton for song search

--- analysis/find_songs.py
+++ analysis/find_songs.py
@@ -6,32 +6,54 @@ import time
 import datetime
 
 try:
-    # fill in your stuff here to update to postgres.
-    # If you don't like postgres, this script can be
-    # easily modified.
+    songs_to_find = ['Bathtub Gin','Tweezer']
+
     connection = psycopg2.connect(
         host="localhost",
         database="postgres",
         user="joe",
         password=""
     )
+    #Still todo:
+        # points for each song to sum up for all the favorite song
+        # 2025-03-22 (discovered) Some songs appear in the same show more than once
+
+
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM setlists")
-
     myresult = cursor.fetchall()
-
-
+    max_count = [0] * len(songs_to_find)
+    counter = [0] * len(songs_to_find)
+    best_week_start = [""] * len(songs_to_find)
+    best_week_end = [""] * len(songs_to_find)
     for x in myresult:
         date_1 = datetime.datetime.strptime(str(x[7]), "%Y-%m-%d")
         show_date = str(x[7])
-        end_date = date_1 + datetime.timedelta(days=10)
+        end_date = date_1 + datetime.timedelta(days=7)
+
         # count songs per date window
-        for i in range(1, 105):
-            str_sql="SELECT song_name FROM setlists WHERE show_date BETWEEN '"
-            str_sql += show_date + "' AND '" + str(end_date) + "'"
-            cursor.execute(str_sql)
-            myresult = cursor.fetchall()
-        print(str(date_1) +"-"+ str(end_date))
+        str_sql="SELECT song_name FROM setlists WHERE show_date BETWEEN '"
+        str_sql += show_date + "' AND '" + str(end_date) + "'"
+        cursor.execute(str_sql)
+        myresult1 = cursor.fetchall()
+
+        start_week = show_date
+        end_week = end_date
+        for i in range(0,len(songs_to_find)):
+            counter[i] = 0
+        for y in myresult1:
+            if (y[0] in songs_to_find):
+                for i in range(0,len(songs_to_find)):
+                    if (y[0] == songs_to_find[i]):
+                        counter[i] += 1
+                        if counter[i] > max_count[i]:
+                            max_count[i] = counter[i]
+                            best_week_start[i] = start_week
+                            best_week_end[i] = end_week
+                            print("new max_counter="+str(max_count[i]))
+                            print("new song_name="+str(y[0]))
+                            print("new week_start="+str(best_week_start[i]))
+                            print("new week_end="+str(best_week_end[i]))
 
 except psycopg2.Error as e:
     print(f"Error interacting with PostgreSQL: {e}")
